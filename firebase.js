ce76336ada7b195bd54d570d7ede573a7b1873aa
NuviOsProject <nuvios.project@gmail.com>
2026-07-11 15:10:42 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,32 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCshZe0JZibINfmOamUrnYPiBfVifNpiaA",
-    authDomain: "nuvios.firebaseapp.com",
-    projectId: "nuvios",
-    storageBucket: "nuvios.firebasestorage.app",
-    messagingSenderId: "595168231407",
-    appId: "1:595168231407:web:81a026964f9e6571d7c01a",
-    measurementId: "G-M1DGCYGZRV"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCshZe0JZibINfmOamUrnYPiBfVifNpiaA",
+  authDomain: "nuvios.firebaseapp.com",
+  projectId: "nuvios",
+  storageBucket: "nuvios.firebasestorage.app",
+  messagingSenderId: "595168231407",
+  appId: "1:595168231407:web:81a026964f9e6571d7c01a",
+  measurementId: "G-M1DGCYGZRV"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function carregarMeta() {
+    const ref = doc(db, "donations", "progress");
+    const snap = await getDoc(ref);
+
+    if (snap.exists()) {
+        console.log(snap.data());
+    } else {
+        console.log("Documento não encontrado.");
+    }
+}
+
+carregarMeta();
